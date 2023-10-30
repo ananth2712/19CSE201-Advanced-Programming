a9c318cbc7ab8724d2877d469ff78473c528628e
Ananth R <116860439+ananth2712@users.noreply.github.com>
2023-10-30 16:19:15 +0530

    Add files via upload

---
+++ python/30th oct/lists.py
@@ -0,0 +1,7 @@
+purchase=["soap","don't","drop","prision",69,"then","face","consequences"]
+print(len(purchase))
+print(purchase[1])
+print(purchase[3])
+print(purchase[-1])
+print(purchase[-2])
+print(purchase[1:-1])   
---
+++ python/30th oct/sort.py
@@ -0,0 +1,10 @@
+details=[]
+details.append("df")
+details.append("df1")
+details.append("df2")
+details.append("df3")
+print(details)
+details.insert(2,"ajith")
+print(details)
+details.remove("ajith")
+print(details)
